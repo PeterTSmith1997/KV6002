ec5894e5b8ad659d5942f3da12ed2c09f8b66397
Peter <p>
2020-01-26 21:25:57 +0000

    Index?

---
+++ system/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peter
+ * Date: 09/12/2019
+ * Time: 19:24
+ */
+function autoloadClasses($className) {
+    $filename = "classes/" . $className . ".php";
+    if (is_readable($filename)) {
+        include $filename;
+    }
+}
+
+spl_autoload_register("autoloadClasses");
+/**$navItems = Array("home"=>"index.php","other page"=>"docs.php","more"=>"more.php");
+$page = new WebPageWithNav('Home','Welcome', $navItems, 'footer');
+echo $page->getPage();
+
+ */
+$page = new Loginpge("login", "welcome", "ABC");
+
+echo $page->getPage();
