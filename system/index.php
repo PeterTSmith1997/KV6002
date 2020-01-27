bb66f4f9e0e771bd3938c2ba2f9a2dd41c196343
Peter <p>
2020-01-27 20:36:05 +0000

    css updates

--- system/index.php
+++ system/index.php
@@ -8,15 +8,12 @@ echo makeHeader("login");
 <form id="login" action="includes/login.php">
     <div class="container">
         <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required>
+        <input type="text" placeholder="Enter Username" name="username" required>
 
         <label for="psw"><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="psw" required>
 
         <button type="submit">Login</button>
-        <label>
-            <input type="checkbox" checked="checked" name="remember"> Remember me
-        </label>
     </div>
 
 </form>
