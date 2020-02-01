cdde4a85f383662e5b0b31ffb963ac4dae1f531a
PeterTSmith1997 <32275665+PeterTSmith1997@users.noreply.github.com>
2020-02-01 16:19:36 +0000

    Update functions.php

--- system/includes/functions.php
+++ system/includes/functions.php
@@ -26,3 +26,66 @@ function makeFooter(){
     </body>
     </html>";
 }
+
+function validate_logon(){
+    /** Create 2 empty arrays to store the input and any errors
+     * Errors are kept as generic as possible in the interest of security*/
+    $input = array();
+    $errors = array();
+    /** Request data from a form and check that there is data else assign a null value */
+    $input['username'] = filter_has_var(INPUT_POST, 'username')?$_POST['username'] : null;
+    $input['password'] = filter_has_var(INPUT_POST, 'password')?$_POST['password'] : null;
+    $errorPassword = false; // stops dipicate error
+    /** Trim both inputs, assumes password does not allow spaces at the end */
+    $input['username'] = trim($input['username']);
+    $input['password'] = trim($input['password']);
+
+    /** if statement to check that both fields have been completed */
+    if ($input['username'] == null ^ $input['password'] == null){
+        $errors[] = "please  provide a user and password";
+        $errorPassword = true;
+    }
+    /** Checks to see if the username or password is empty, either both or each of them
+     * and also checks if there is already error from the above if statement*/
+    if ((empty($input['username']) ^ empty($input['password'])) && $errorPassword == false){
+        $errors[] = "please  provide a user and password";
+    }
+
+    $dbConn = getConnection();
+    /** sql to select the password and first name from the database */
+    $sql    = "select passwordHash, firstname
+            from nbc_users
+            WHERE username = :username";
+    $stmt   = $dbConn->prepare($sql);
+    $stmt->execute(array(':username' => $input['username']));
+
+    $recordObj = $stmt->fetchObject();
+    /** If statement to see if a row is returned */
+    if ($recordObj) {
+        $passwordHash = $recordObj->passwordHash;
+        /** Use password verify to make sure the password is correct and store data in the session */
+        if (password_verify($input['password'], $passwordHash)) {
+            $input['name'] = $recordObj->firstname;
+            $_SESSION['user']     = $input['user'];
+            $_SESSION['fName']    = $input['name'];
+            $_SESSION['loggedIn'] = true;
+            $_SESSION['lastTime'] = time(); // Use to check for inactivity
+
+        }
+        /** If the password can not be verified */
+        else {
+            $errors[] = "unknown user / password";
+        }
+    }
+    /** If there is no record returned */
+    else {
+        $errors[] = "unknown user / password";
+    }
+    /** Stores the page that they logged in from */
+    $input['page'] = $_SERVER['HTTP_REFERER'];
+
+    return array($input, $errors);
+}
+function store_errors($errors){
+    $_SESSION['errors'] = $errors;
+}
