3939c16d36286f6343b2fd2c0998aa7f44f68102
Peter <p>
2020-02-07 21:18:22 +0000

    Tweaks

---
+++ system/editShift.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+require_once 'includes/functions.php';
+echo makeHeader('View shifts');
+if ($_SESSION['type'] !== 'su') {
+    echo "<p> not part of system</p>";
+    echo makeFooter();
+} else{
+
+}
