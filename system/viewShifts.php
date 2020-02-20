c6f547c9243947b41b51dda5634dd326dc464057
Peter <p>
2020-02-20 22:08:29 +0000

    clean up code

--- system/viewShifts.php
+++ system/viewShifts.php
@@ -9,11 +9,9 @@ if ($_SESSION['type'] !== 'su'){
 } else{
     //nav bar needed here
     echo "<div class='container'>
- <div  class='col-md3 offset-md-3' id='welcome'> Welcome". $_SESSION['fName'] ."</div>
+ <div  class='col-md3 offset-md-3' id='welcome'> Welcome ". $_SESSION['fName'] ."</div>
 </div>";
-    echo getShiftsAllocated();
-    echo makeBookingForm();
-    echo showErrors();
+    echo getShiftsAllocated(). makeBookingForm(). showErrors();
 }
 
 echo makeFooter();
