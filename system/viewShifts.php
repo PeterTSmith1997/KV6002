77b55695223e49a6e781cc10c5dbc9cb254d106b
peter.t.smith <w16018262@northumbria.ac.uk>
2020-02-26 10:45:13 +0000

    Will this work?

--- system/viewShifts.php
+++ system/viewShifts.php
@@ -2,6 +2,16 @@
 session_start();
 require_once 'includes/functions.php';
 echo makeHeader('View shifts');
+?>
+<script type="application/javascript">
+    function confirmationDelete(anchor)
+    {
+        var conf = confirm('Are you sure want to delete this record?');
+        if(conf)
+            window.location=anchor.attr("href");
+    }
+</script>
+<?php
 if ($_SESSION['type'] !== 'su'){
     echo "<p> not part of system</p>";
     echo makeFooter();
@@ -13,14 +23,5 @@ if ($_SESSION['type'] !== 'su'){
 </div>";
     echo getShiftsAllocated(). makeBookingForm(). showErrors();
 }
-?>
-<script type="application/javascript">
-    function confirmationDelete(anchor)
-    {
-        var conf = confirm('Are you sure want to delete this record?');
-        if(conf)
-            window.location=anchor.attr("href");
-    }
-</script>
-<?php
+
 echo makeFooter();
