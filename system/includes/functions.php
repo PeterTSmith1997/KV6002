d15c4ebae3b129bdd8fc53d52ec3f14ee2440bac
PeterTSmith1997 <32275665+PeterTSmith1997@users.noreply.github.com>
2020-03-02 18:36:52 +0000

    Nav doone css

--- system/includes/functions.php
+++ system/includes/functions.php
@@ -476,14 +476,26 @@ function deleteShift(){
 }
 
 function getNav(){
-    $nav = "<nav class=\"navbar sticky-top navbar-light bg-light\">
-                <div class='collapse navbar-collapse' id='navbarNav'>
+    $nav = "<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">
+  <a class=\"navbar-brand\" href=\"#\">Shift booker</a>
+  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" 
+  aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+    <span class=\"navbar-toggler-icon\"></span>
+  </button>
+
+  <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+    <ul class=\"navbar-nav mr-auto\">
+      <li class=\"nav-item\">
+        <a class=\"nav-link\" href="#">Home</a>
+      </li>
+      <li class=\"nav-item\">
+        <a class=\"nav-link\" href=\"#\">Change pass</a>
+      </li>
       
-        <ul class='navbar-nav'>
-            <li class='nav-item'><a class='nav-link' href='changePassword.php'> Password stuff</a></li>
-            <li class='nav-item'><a class='nav-link' href='logout.php'>Logout</a></li>
-            </ul>              
-</div>
- </nav>\n ";
+      
+    </ul>
+    
+  </div>
+</nav>\n ";
     return $nav;
 }
